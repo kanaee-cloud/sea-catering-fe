07b5d43eaf635316e32e9d31eb82cf6ff6dbe17e
aaltansa <arsalnaufal2007@gmail.com>
2025-06-19 14:30:49 +0700

    [+] First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+       <h1 className='text-3xl text-red-600'>tes</h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
